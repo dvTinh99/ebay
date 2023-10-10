85fc1823d90973c2323dc799f197340bb85c9ac5
guen <heloo@guen.dev>
2023-10-10 16:07:08 +0700

    fix missing get

--- resources/views/shop2/page/shops-create.blade.php
+++ resources/views/shop2/page/shops-create.blade.php
@@ -21,7 +21,7 @@
             <div class="col-lg-6">
                 <ul class="breadcrumb bg-transparent p-0 justify-content-center justify-content-lg-end">
                     <li class="breadcrumb-item opacity-50">
-                        <a class="text-reset" href="https://www.ebeebbuy.cc">Trang Chủ</a>
+                        <a class="text-reset" href="/">Trang Chủ</a>
                     </li>
                     <li class="text-dark fw-600 breadcrumb-item">
                         <a class="text-reset" href="create.html">"Đăng ký cửa hàng của bạn"</a>
