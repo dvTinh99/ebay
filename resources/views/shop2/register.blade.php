4d71ba4e4b7f578e627c9ce5b22c19cd97491fdf
guen <heloo@guen.dev>
2023-10-12 22:21:51 +0700

    cm

--- resources/views/shop2/register.blade.php
+++ resources/views/shop2/register.blade.php
@@ -1,15 +1,11 @@
 @include('shop2.layout.header')
-<div class="modal fade" id="order_details" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
+<div class="modal fade" id="order_details" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
         <div class="modal-content">
-            <div id="order-details-modal-body">
-
-            </div>
+            <div id="order-details-modal-body"></div>
         </div>
     </div>
 </div>
-
 
 <section class="gry-bg py-4">
     <div class="profile">
@@ -19,47 +15,58 @@
                     <div class="card">
                         <div class="text-center pt-4">
                             <h1 class="h4 fw-600">
-                                tạo tài khoản
+                                Tạo tài khoản
                             </h1>
                         </div>
+
+                        @if (session('error'))
+                            <div class="alert alert-danger">
+                                {{ session('error') }}
+                            </div>
+                        @endif
+
                         <div class="px-4 py-3 py-lg-4">
                             <div class="">
-                                <form id="reg-form" class="form-default" role="form"
-                                    action="/user/register" method="POST">
+                                <form id="reg-form" class="form-default" role="form" action="/user/register" method="POST">
                                     @csrf
                                     <div class="form-group">
-                                        <input type="text" class="form-control" value=""
-                                            placeholder="Họ và tên" name="name">
+                                        <input type="text" class="form-control" value="" placeholder="Họ và tên" name="name" />
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="email" class="form-control" value="" placeholder="E-mail"
-                                            name="email">
-                                        <input type="hidden" class="form-control" value="customer" placeholder="E-mail"
-                                            name="role">
+                                        <input type="email" class="form-control" value="" placeholder="E-mail" name="email" />
+                                        <input type="hidden" class="form-control" value="customer" placeholder="E-mail" name="role" />
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="password" class="form-control" placeholder="Mật khẩu"
-                                            name="password">
+                                        <input type="password" class="form-control" placeholder="Mật khẩu" name="password" />
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="password" class="form-control" placeholder="Xác nhận mật khẩu"
-                                            name="password_confirmation">
+                                        <input type="password" class="form-control" placeholder="Xác nhận mật khẩu" name="password_confirmation" />
                                     </div>
+
+                                    <div class="form-group">
+                                        <input
+                                            required
+                                            type="text"
+                                            class="form-control"
+                                            value="{{ $_GET['invitation_code'] ?? '' }}"
+                                            placeholder="Mã mời"
+                                            name="ref_link"
+                                        />
+                                    </div>
+
                                     <div class="mb-3">
                                         <label class="aiz-checkbox">
-                                            <input type="checkbox" name="checkbox_example_1" required>
-                                            <span class=opacity-60>Bằng cách đăng ký, bạn đồng ý với các điều
-                                                khoản và điều kiện của chúng tôi.</span>
+                                            <input type="checkbox" name="checkbox_example_1" required />
+                                            <span class="opacity-60">Bằng cách đăng ký, bạn đồng ý với các điều khoản và điều kiện của chúng tôi.</span>
                                             <span class="aiz-square-check"></span>
                                         </label>
                                     </div>
 
                                     <div class="mb-5">
-                                        <button type="submit" class="btn btn-primary btn-block fw-600">Tạo
-                                            tài khoản</button>
+                                        <button type="submit" class="btn btn-primary btn-block fw-600">Tạo tài khoản</button>
                                     </div>
                                 </form>
                             </div>
@@ -70,7 +77,6 @@
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
