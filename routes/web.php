e53569c06a253ce06781ad2262137816143da1b6
guen <heloo@guen.dev>
2023-10-12 18:47:23 +0700

    cm

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::domain(env('DOMAIN_SHOP','shop.btsdoors.com'))->group(function () {
     Route::get('/', [ShopController::class, 'index']);
     Route::get('/users/login', [ShopController::class, 'login']);
